0a4e7d565f19d367e7a4626662bbf2557fefc3a2
DARSEN12 <faragonc@unsa.edu.pe>
2024-12-11 12:00:04 -0500

    Culminando las historias e implementando GUI

--- Main.java
+++ Main.java
@@ -5,6 +5,17 @@ public class Main {
 
         Mapa mapa = new Mapa(10, 10);
 
+        System.out.println("\n");
+
+        System.out.println("En un mundo donde las ambiciones de poder y gloria dominan los corazones de los gobernantes, "
+                         + "los antiguos reinos han decidido enfrentar su destino en el campo de batalla.\n"
+                         + "Desde las frías tierras de los Reinos Escandinavos, pasando por los férreos Reinos Anglo-Sajones y el noble Reino de Asturias,\n"
+                         + "hasta los imponentes dominios del Imperio Bizantino, el Imperio Carolingio y el Reino de los Búlgaros, las tensiones han alcanzado un punto crítico.\n"
+                         + "Cada reino busca consolidar su supremacía en un mundo dividido, utilizando estrategias militares, unidades especializadas y el dominio del territorio como sus principales armas.\n"
+                         + "Las guerras han comenzado, y solo uno podrá reclamar la hegemonía sobre estas tierras conflictivas.");
+
+        System.out.println("\n");
+
         String[] reinosSeleccionados = Reino.escogerReinos();
 
         System.out.println("Jugador 1 ha elegido: " + reinosSeleccionados[0]);
@@ -16,10 +27,10 @@ public class Main {
         System.out.println("------------------------------------------");
 
         Ejercito ejercito1 = new Ejercito(reinosSeleccionados[0]);
-        ejercito1.generarSoldados(3);
+        ejercito1.generarSoldados(1);
 
         Ejercito ejercito2 = new Ejercito(reinosSeleccionados[1]);
-        ejercito2.generarSoldados(3);
+        ejercito2.generarSoldados(1);
 
         ejercito1.mostrarSoldados();
         System.out.println("------------------------------------------");
